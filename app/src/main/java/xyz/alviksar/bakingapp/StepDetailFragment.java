3eda414919998018088c043c5fb43eacf278464d
Alexey Sarafannikov <alviksar@gmail.com>
2018-04-27 23:02:14 +0300

    Modify Player control

--- app/src/main/java/xyz/alviksar/bakingapp/StepDetailFragment.java
+++ app/src/main/java/xyz/alviksar/bakingapp/StepDetailFragment.java
@@ -1,5 +1,6 @@
 package xyz.alviksar.bakingapp;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -131,6 +132,7 @@ public class StepDetailFragment extends Fragment {
                         .createMediaSource(Uri.parse(mVideoUrl));
                 mPlayer.prepare(videoSource);
 
+                hideSystemUi();
                 mPlayer.setPlayWhenReady(mPlayWhenReadyState);
                 if (mPlaybackPosition != C.TIME_UNSET)
                     mPlayer.seekTo(mCurrentWindow, mPlaybackPosition);            }
@@ -151,19 +153,17 @@ public class StepDetailFragment extends Fragment {
      */
     private void releasePlayer() {
         if (mPlayer != null) {
-            mPlayer.stop();
             getPlaybackState();
             mPlayer.release();
             mPlayer = null;
         }
     }
 
-
+/*
     @Override
     public void onResume() {
         super.onResume();
-        if (mVideoUrl != null)
-            initializePlayer();
+        initializePlayer();
     }
 
     @Override
@@ -171,7 +171,7 @@ public class StepDetailFragment extends Fragment {
         super.onPause();
         releasePlayer();
     }
-
+*/
     public String getDescription() {
         return mDescription;
     }
@@ -180,4 +180,46 @@ public class StepDetailFragment extends Fragment {
         this.mDescription = mDescription;
     }
 
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mPlayerView == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
 }
